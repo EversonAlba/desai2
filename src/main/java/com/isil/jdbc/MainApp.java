1cec9ae357e6e1ed0dd4312340a6c992f6266846
Everson <eversonalba98@hotmail.com>
2022-09-10 08:40:48 -0500

    Primer commit

---
+++ src/main/java/com/isil/jdbc/MainApp.java
@@ -0,0 +1,33 @@
+package com.isil.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MainApp {
+
+    public static void main(String[] args) throws Exception {
+        String jdbcUrl= "jdbc:mysql://localhost:3306/DESAI";
+
+        //Cargar el driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        //Crear la conexion
+        Connection con = DriverManager.getConnection(jdbcUrl, "root", "root");
+
+        //Crear el statemet
+        Statement st = con.createStatement();
+
+        //Ejecutar la sentencia SQL
+        ResultSet rs= st.executeQuery("select * from Users");
+
+        //procesar el resultado
+        while(rs.next()){
+            System.out.println(rs.getString("idUser") + " " + rs.getString("name")
+                    + " " + rs.getString("phone") + " " + rs.getString("city"));
+        }
+
+
+    }
+}
